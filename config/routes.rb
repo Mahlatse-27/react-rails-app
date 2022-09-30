df7f94b041df58179a2e1eb0ee9019894ff0fd99
Mahlatse_27 <mahlatse.morapedi@supplychainpartner.com>
2022-09-30 13:35:40 +0200

    Small change 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homepage#index'
   get 'homepage/create'
+
+  
   get 'homepage/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
